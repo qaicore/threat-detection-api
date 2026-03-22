d5edfb8bb209330fdd0a8411c0f04c6113e58b30
sadiq t <staslimrt@gmail.com>
2026-03-22 15:33:05 -0400

    Added indicator and case endpoints

--- auth.py
+++ auth.py
@@ -2,7 +2,17 @@ from passlib.context import CryptContext
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from database import get_db
+from models import User
 import os
+
+from models import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
 
 
 load_dotenv()
@@ -30,3 +40,14 @@ def decode_token(token: str) -> dict:
         return payload
     except JWTError:
         return None
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    result = decode_token(token)
+    if result is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
+    email = result["sub"]
+    check_user = db.query(User).filter(User.email == email).first()
+    if check_user is None:
+        raise HTTPException(status_code=401, detail="Invalid user")
+    return check_user
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
+from multiprocessing import current_process
 from fastapi import FastAPI, Depends
 from fastapi import HTTPException
 from database import engine
-from models import Base, User
-from schemas import UserCreate, UserResponse
+from models import Base, Case, User, Indicator
+from schemas import UserCreate, UserResponse, IndicatorCreate, CaseCreate
 from database import get_db
-from auth import hash_password, verify_password, create_token
+from auth import hash_password, verify_password, create_token, get_current_user
 from sqlalchemy.orm import Session
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 @app.get("/health")
 def health():
     return {"status": "ok"}
@@ -40,4 +40,38 @@ def login(user: UserCreate, db: Session = Depends(get_db)):
     user_token = create_token({"sub": existing_user.email})
     return user_token
     
-   
+@app.post("/indicators")
+def create_indicator(indicator: IndicatorCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    new_indicator = Indicator(value = indicator.value, type = indicator.type, severity = indicator.severity, notes = indicator.notes, tags = indicator.tags )
+    db.add(new_indicator)
+    db.commit()
+    db.refresh(new_indicator)
+    return new_indicator
+
+@app.get("/indicators")
+def list_indicators(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    indicators = db.query(Indicator).all()
+    return indicators
+
+@app.get("/indicators/{id}")
+def id_indicator(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    indicator = db.query(Indicator).filter(Indicator.id == id).first()
+    return indicator
+
+@app.post("/cases")
+def create_case(case: CaseCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    new_case = Case(title = case.title, description = case.description, severity = case.severity)
+    db.add(new_case)
+    db.commit()
+    db.refresh(new_case)
+    return new_case
+
+@app.get("/cases")
+def list_cases(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    cases = db.query(Case).all()
+    return cases
+
+@app.get("/cases/{id}")
+def id_case(id:int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    case = db.query(Case).filter(Case.id == id).first()
+    return case
